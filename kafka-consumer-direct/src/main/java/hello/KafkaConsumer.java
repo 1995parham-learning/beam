690f32628be935c85ac02f6bef89a5e046ecb5e4
Elahe Dastan <elahe.dstn@gmail.com>
2022-08-08 23:49:23 +0430

    fix: correct merge conflicts

--- kafka-consumer-direct/src/main/java/hello/KafkaConsumer.java
+++ kafka-consumer-direct/src/main/java/hello/KafkaConsumer.java
@@ -1,17 +1,23 @@
 package hello;
 
-import com.google.common.collect.ImmutableMap;
-
 import org.apache.beam.runners.direct.DirectRunner;
-//import org.apache.beam.runners.spark.io.ConsoleIO;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Keys;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.values.KV;
 import org.apache.kafka.common.serialization.StringDeserializer;
+
+import com.google.common.collect.ImmutableMap;
 
 public class KafkaConsumer {
     public interface KafkaConsumerOptions extends PipelineOptions {
@@ -50,7 +56,6 @@ public class KafkaConsumer {
 
                         .withoutMetadata() // PCollection<KV<Long, String>>
                 )
-<<<<<<< HEAD:kafka-consumer-direct/src/main/java/hello/KafkaConsumer.java
                 .apply(Values.<String>create())
                 .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
                     @ProcessElement
@@ -69,12 +74,7 @@ public class KafkaConsumer {
                         return input.getKey() + ": " + input.getValue();
                     }
                 }))
-                        .apply(TextIO.write().to("don't-use-beam"));
-//                .apply(ConsoleIO.Write.out());
-=======
-                .apply(Keys.create())
-                .apply(ConsoleIO.Write.out());
->>>>>>> 45198d6cda88c179c5efa9841835a5b88dc3c44e:kafka-consumer-spark/src/main/java/hello/KafkaConsumer.java
+                .apply(TextIO.write().to("don't-use-beam"));
 
         p.run().waitUntilFinish();
     }
