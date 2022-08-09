1979d62b69486874c9dfddea286c745bb0f43c84
Elahe Dastan <elahe.dstn@gmail.com>
2022-08-09 12:40:06 +0430

    feat: formats the java codes

--- kafka-consumer-direct/src/main/java/hello/KafkaConsumer.java
+++ kafka-consumer-direct/src/main/java/hello/KafkaConsumer.java
@@ -20,62 +20,65 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import com.google.common.collect.ImmutableMap;
 
 public class KafkaConsumer {
-    public interface KafkaConsumerOptions extends PipelineOptions {
-      @Description("kafka bootstrap servers")
-      @Default.String("127.0.0.1:9094")
-      String getBootstrapServers();
+  public interface KafkaConsumerOptions extends PipelineOptions {
+    @Description("kafka bootstrap servers")
+    @Default.String("127.0.0.1:9094")
+    String getBootstrapServers();
 
-      void setBootstrapServers(String value);
+    void setBootstrapServers(String value);
+  }
+
+  static final String TOKENIZER_PATTERN = "[^\\p{L}]+";
+
+  public static void main(String[] args) {
+    System.out.println("Welcome to Elahe beam testing");
+    for (String arg : args) {
+      System.out.println(arg);
     }
 
-    static final String TOKENIZER_PATTERN = "[^\\p{L}]+";
+    KafkaConsumerOptions options = PipelineOptionsFactory.fromArgs(args).withValidation()
+        .as(KafkaConsumerOptions.class);
+    options.setRunner(DirectRunner.class);
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Elahe beam testing");
-        for (String arg: args) {
-          System.out.println(arg);
-        }
+    // Create the Pipeline object with the options we defined above.
+    Pipeline p = Pipeline.create(options);
 
-        KafkaConsumerOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(KafkaConsumerOptions.class);
-        options.setRunner(DirectRunner.class);
+    p.apply(KafkaIO.<String, String>read()
+        .withBootstrapServers(options.getBootstrapServers())
+        .withTopic("dls-elahe")
+        .withKeyDeserializer(StringDeserializer.class)
+        .withValueDeserializer(StringDeserializer.class)
+        .withConsumerConfigUpdates(ImmutableMap.of("auto.offset.reset", (Object) "earliest"))
 
-        // Create the Pipeline object with the options we defined above.
-        Pipeline p = Pipeline.create(options);
+        // We're writing to a file, which does not support unbounded data sources. This
+        // line makes it bounded to
+        // the first 5 records.
+        // In reality, we would likely be writing to a data source that supports
+        // unbounded data, such as BigQuery.
+        // .withMaxNumRecords(5)
 
-        p.apply(KafkaIO.<String, String>read()
-                        .withBootstrapServers(options.getBootstrapServers())
-                        .withTopic("dls-elahe")
-                        .withKeyDeserializer(StringDeserializer.class)
-                        .withValueDeserializer(StringDeserializer.class)
-                        .withConsumerConfigUpdates(ImmutableMap.of("auto.offset.reset", (Object) "earliest"))
+        .withoutMetadata() // PCollection<KV<Long, String>>
+    )
+        .apply(Values.<String>create())
+        .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
+          @ProcessElement
+          public void processElement(ProcessContext c) {
+            for (String word : c.element().split(TOKENIZER_PATTERN)) {
+              if (!word.isEmpty()) {
+                c.output(word);
+              }
+            }
+          }
+        }))
+        .apply(Count.<String>perElement())
+        .apply("FormatResults", MapElements.via(new SimpleFunction<KV<String, Long>, String>() {
+          @Override
+          public String apply(KV<String, Long> input) {
+            return input.getKey() + ": " + input.getValue();
+          }
+        }))
+        .apply(TextIO.write().to("don't-use-beam"));
 
-                        // We're writing to a file, which does not support unbounded data sources. This line makes it bounded to
-                        // the first 5 records.
-                        // In reality, we would likely be writing to a data source that supports unbounded data, such as BigQuery.
-                        // .withMaxNumRecords(5)
-
-                        .withoutMetadata() // PCollection<KV<Long, String>>
-                )
-                .apply(Values.<String>create())
-                .apply("ExtractWords", ParDo.of(new DoFn<String, String>() {
-                    @ProcessElement
-                    public void processElement(ProcessContext c) {
-                        for (String word : c.element().split(TOKENIZER_PATTERN)) {
-                            if (!word.isEmpty()) {
-                                c.output(word);
-                            }
-                        }
-                    }
-                }))
-                .apply(Count.<String>perElement())
-                .apply("FormatResults", MapElements.via(new SimpleFunction<KV<String, Long>, String>() {
-                    @Override
-                    public String apply(KV<String, Long> input) {
-                        return input.getKey() + ": " + input.getValue();
-                    }
-                }))
-                .apply(TextIO.write().to("don't-use-beam"));
-
-        p.run().waitUntilFinish();
-    }
+    p.run().waitUntilFinish();
+  }
 }
