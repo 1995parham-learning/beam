b065ccd4fc0927a13d57a49c36f2a9ff5b1797e8
Elahe Dastan <elahe.dstn@gmail.com>
2022-08-21 00:35:40 +0430

    feat: even tests are working :dancer:

--- transformations/src/test/java/transformations/TransformationsTest.java
+++ transformations/src/test/java/transformations/TransformationsTest.java
@@ -1,30 +1,74 @@
 package transformations;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.util.List;
 
-import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.testing.NeedsRunner;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.values.PCollection;
 
 public class TransformationsTest {
-  @Test
-  public void dlsParsing() {
-    List<String> dls = List.of(
-        "{ \"id\": 1378, \"timestamp\": \"2022-08-16T09:12:43.431351502Z\" }",
-        "{ \"id\": 7, \"timestamp\": \"2022-08-16T09:12:43.431351502Z\" }",
-        "{ \"id\": 20, \"timestamp\": \"2022-08-16T09:12:43.431351502Z\" }");
+  @Rule
+  public TestPipeline p = TestPipeline.create();
 
-    Pipeline p = TestPipeline.create();
+  @Test
+  public void dlsParsing() throws Exception {
+    List<String> dls = List.of(
+        """
+            {
+              "id":7,
+              "lat":38.241,
+              "lng":48.314884,
+              "bearing":120,
+              "accuracy":68,
+              "timestamp":"2022-08-20T14:33:58.238766989Z",
+              "serviceType":"ECO",
+              "status":"AVAILABLE",
+              "speed":7,
+              "deviceTimestamp":"2022-08-20T14:33:59Z"
+            }
+                    """,
+        """
+            {
+              "id":20,
+              "lat":38.241,
+              "lng":48.314884,
+              "bearing":120,
+              "accuracy":68,
+              "timestamp":"2022-08-20T14:33:58.238766989Z",
+              "serviceType":"ECO",
+              "status":"AVAILABLE",
+              "speed":7,
+              "deviceTimestamp":"2022-08-20T14:33:59Z"
+            }
+            """,
+        """
+            {
+              "id":1378,
+              "lat":38.241,
+              "lng":48.314884,
+              "bearing":120,
+              "accuracy":68,
+              "timestamp":"2022-08-20T14:33:58.238766989Z",
+              "serviceType":"ECO",
+              "status":"AVAILABLE",
+              "speed":7,
+              "deviceTimestamp":"2022-08-20T14:33:59Z"
+            }
+            """);
 
     PCollection<String> output = p.apply(Create.of(dls)).apply(Transform.on());
 
     PAssert.that(output).containsInAnyOrder(
-        "DriverLocation [1378]",
-        "DriverLocation [7]",
-        "DriverLocation [20]");
+        "DriverLocation [1378] @ 2022-08-20T14:33:59.000Z",
+        "DriverLocation [7] @ 2022-08-20T14:33:59.000Z",
+        "DriverLocation [20] @ 2022-08-20T14:33:59.000Z");
+
+    p.run().waitUntilFinish();
   }
 }
