2e5b899738d498aa8a926c122fcd466b518b8efc
Elahe Dastan <elahe.dastan@snapp.cab>
2022-08-20 13:42:31 +0430

    Add all fields to DriveLocation

--- transformations/src/main/java/transformations/DriverLocation.java
+++ transformations/src/main/java/transformations/DriverLocation.java
@@ -8,6 +8,12 @@ public class DriverLocation implements Serializable {
   private Instant timestamp;
   private double lat;
   private double lng;
+  private double bearing;
+  private double accuracy;
+  private double serviceType;
+  private double status;
+  private double speed;
+  private double deviceTimestamp;
 
   public long getId() {
     return id;
@@ -44,5 +50,53 @@ public class DriverLocation implements Serializable {
 
   public void setLng(double lng) {
     this.lng = lng;
+  }
+
+  public double getBearing() {
+    return bearing;
+  }
+
+  public void setBearing(double bearing) {
+    this.bearing = bearing;
+  }
+
+  public double getAccuracy() {
+    return accuracy;
+  }
+
+  public void setAccuracy(double accuracy) {
+    this.accuracy = accuracy;
+  }
+
+  public double getServiceType() {
+    return serviceType;
+  }
+
+  public void setServiceType(double serviceType) {
+    this.serviceType = serviceType;
+  }
+
+  public double getStatus() {
+    return status;
+  }
+
+  public void setStatus(double status) {
+    this.status = status;
+  }
+
+  public double getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(double speed) {
+    this.speed = speed;
+  }
+
+  public double getDeviceTimestamp() {
+    return deviceTimestamp;
+  }
+
+  public void setDeviceTimestamp(double deviceTimestamp) {
+    this.deviceTimestamp = deviceTimestamp;
   }
 }
