83989550e5070b2f8b05d1a3c85abf06d47b52cb
Parham Alvani <parham.alvani@gmail.com>
2022-08-23 09:25:50 +0430

    fix: remove scala non-serializable module

--- transformations/src/main/java/transformations/Transform.java
+++ transformations/src/main/java/transformations/Transform.java
@@ -4,20 +4,11 @@ import org.apache.beam.sdk.coders.SerializableCoder;
 import org.apache.beam.sdk.extensions.jackson.ParseJsons;
 import org.apache.beam.sdk.values.PCollection;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ToString;
 
 public class Transform extends PTransform<PCollection<String>, PCollection<String>> {
-  private final ObjectMapper mapper = JsonMapper.builder()
-      .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
-      .findAndAddModules()
-      .build();
-
-  private final ParseJsons<DriverLocation> pj = ParseJsons.of(DriverLocation.class).withMapper(mapper);
+  private final ParseJsons<DriverLocation> pj = ParseJsons.of(DriverLocation.class);
 
   public static Transform on() {
     return new Transform();
